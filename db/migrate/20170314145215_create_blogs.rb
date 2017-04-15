a5501971ab5056814e475248bf82e73a06d1331d
Lisa Go <igoqueen5@gmail.com>
2017-04-15 13:33:21 +0000

    first commit

---
+++ db/migrate/20170314145215_create_blogs.rb
@@ -0,0 +1,13 @@
+class CreateBlogs < ActiveRecord::Migration
+  def change
+    create_table :blogs do |t|
+      t.string :title
+      t.text :content
+      t.string :name
+      t.string :email
+      t.text   :content
+
+      t.timestamps null: false
+    end
+  end
+end
