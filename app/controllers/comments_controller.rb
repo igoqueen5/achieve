d17fbe0db108b7c7660237b1530e2770d7932865
igoqueen5 <igoqueen5@gmail.com>
2017-06-24 07:00:56 +0000

    create comment function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,33 +1,27 @@
 class CommentsController < ApplicationController
 
-  def index
-    @blogs = Blog.all
-    respond_to do |format|
-      format.html
-      format.js
-    end
-  end
 
   def create
+    # Blogをパラメータの値から探し出し,Blogに紐づくcommentsとしてbuildします。
     @comment = current_user.comments.build(comment_params)
     @blog = @comment.blog
-
+    # クライアント要求に応じてフォーマットを変更
     respond_to do |format|
       if @comment.save
-        format.html{redirect_to blog_path(@blog), notice: 'コメントを投稿しました。'}
-        format.js{render :index}
+        format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
       else
-        format.html{render :new}
+        format.html { render :new }
       end
     end
+  end
 
+  def destroy
+    @comment = Comment.find(params[:id])
     respond_to do |format|
-     if @comment.delete
+    @comment.destroy
       format.html{redirect_to blog_path(@blog), notice: 'コメントを削除しました。'}
       format.js{render :index}
-    else
-      format.html{render :new}
-  end
+    end
 end
 
   private
